3df2d74cf26bc3eca2b004a3608bb6d0eb019857
小野咲みなみ <mnmonzk@gmail.com>
2017-12-24 21:18:24 +0900

    🎉 初コミット

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    include "config.php";
+    $title = "トップページ";
+    include "header.php";
+?>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                </div>
+<?php include "sidebar.php"; ?>
+            </div>
+        </div>
+<?php include "footer.php"; ?>
