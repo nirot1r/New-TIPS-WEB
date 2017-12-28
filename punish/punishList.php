bd3c1aa89d2d0c51c19591ffea1bad5b4435cc4a
小野咲みなみ <mnmonzk@gmail.com>
2017-12-28 22:35:31 +0900

    ✏ 処罰リストのファイル作成

---
+++ punish/punishList.php
@@ -0,0 +1,13 @@
+<?php
+    include "../inc/config.php";
+    $title = "処罰一覧";
+    include "../inc/header.php";
+?>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                </div>
+<?php include "../inc/sidebar.php"; ?>
+            </div>
+        </div>
+<?php include "../inc/footer.php"; ?>
